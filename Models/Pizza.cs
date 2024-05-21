85136f45905166ecbb13246edc6c844ce9ac4b12
aeon <fabio-trab@outlook.com>
2024-05-21 15:30:53 -0400

    feat: change properties of pizza

--- Models/Pizza.cs
+++ Models/Pizza.cs
@@ -1,8 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace ContosoPizza.Models;
 
 public class Pizza
 {
     public int Id { get; set; }
+
+    [Required]
+    [MaxLength(100)]
     public string? Name { get; set; }
+
+    [Required]
     public bool IsGlutenFree { get; set; }
 }
