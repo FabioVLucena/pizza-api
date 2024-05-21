3b42f74e5f150c2417adb3efd298ffb9cf7272c6
aeon <fabio-trab@outlook.com>
2024-05-21 15:22:26 -0400

    feat: add dbcontext and sqlconnection

---
+++ Data/PizzaContext.cs
@@ -0,0 +1,9 @@
+using ContosoPizza.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContosoPizza.Data;
+
+public class PizzaContext(DbContextOptions<PizzaContext> options) : DbContext(options)
+{
+    public DbSet<Pizza> Pizzas => Set<Pizza>();
+}
--- obj/Debug/net8.0/ContosoPizza.AssemblyInfo.cs
+++ obj/Debug/net8.0/ContosoPizza.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContosoPizza")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a3925c46942fb3ea535b2cac9103da225b9c069")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee0bd2dc3bad368d8d4e73abab7f55d821f71135")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContosoPizza")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ContosoPizza")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
